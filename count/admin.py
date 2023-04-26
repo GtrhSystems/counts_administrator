254e6b401ceea32f26fb83a3af468ad78d2c3ef5
juank <ingjuancarlosmoreno@gmail.com>
2023-04-26 11:44:21 -0500

    Creacion de plataformas, edicio y edicion de clientes

--- count/admin.py
+++ count/admin.py
@@ -1,11 +1,11 @@
-from django.contrib import admin
-from .models import Platform, Count
+#from django.contrib import admin
+#from .models import Platform, Count
 
-@admin.register(Platform)
-class PlatformAdmin(admin.ModelAdmin):
-   list_display = ( 'name','logo','num_profiles', 'active' )
-   fields  = ['name', 'logo', 'num_profiles' ,'active']
-   list_filter = ('name','active') #se aplican filtros por estos dos campos
+#@admin.register(Platform)
+#class PlatformAdmin(admin.ModelAdmin):
+#   list_display = ( 'name','logo','num_profiles', 'active' )
+#   fields  = ['name', 'logo', 'num_profiles' ,'active']
+#   list_filter = ('name','active') #se aplican filtros por estos dos campos
 
 #@admin.register(Count)
 #class CountAdmin(admin.ModelAdmin):

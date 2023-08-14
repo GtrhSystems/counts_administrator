10fdf0f20ddf4c40478cc61c5bdc3ef3b16c27c4
root <root@vmi1287178.contaboserver.net>
2023-08-14 16:51:16 +0200

    Se agrego start.sh

---
+++ start.sh
@@ -0,0 +1,22 @@
+!/bin/bash
+
+NAME="Administrador_de_cuentas"  # Name of the application
+DJANGODIR=/home/counts_adminitration  # Django project directory
+NUM_WORKERS=3
+DJANGOENVDIR=$DJANGODIR/env  # Django project env
+DJANGO_SETTINGS_MODULE=count_admin_project.settings
+DJANGO_WSGI_MODULE=count_admin_project.wsgi 
+#export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+#export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+echo "Starting $NAME as `whoami`"
+NUM_WORKERS=5
+# Activate the virtual environment
+
+. $DJANGOENVDIR/bin/activate
+#source /home/ubuntu/webapp/myproject/proj/.env
+#export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+# Start gunicorn
+0
+exec gunicorn ${DJANGO_WSGI_MODULE}:application --name $NAME --workers $NUM_WORKERS --bind 0.0.0.0:8000

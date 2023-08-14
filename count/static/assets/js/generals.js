e42aea3a8680f1789f2fa7857c671f6623f5f0ce
juator <juator@hotmail.com>
2023-08-14 14:16:43 -0500

    Ajuste de enlace de vender

--- count/static/assets/js/generals.js
+++ count/static/assets/js/generals.js
@@ -4,32 +4,33 @@ $('.select2-selection').css('height','43px')
 $('body').on("submit", "form" , function(event){
         $(this).find('input[type="submit"]').prop("disabled", true)
 })
-
-$('.table-normal').DataTable( {
-      responsive: true,
-      ordering: false,
-      language: {
-        processing:     "Procesamiento en courso...",
-        search:         "Buscar&nbsp;:",
-        lengthMenu:     "Mostrar _MENU_ &eacute;l&eacute;mentos",
-        info:           "Mostrar de elelemento _START_ al _END_,     Total _TOTAL_ ",
-        infoEmpty:      "Visualización del elemento 0 a 0 de 0 artículos",
-        infoFiltered:   "(filtrados _MAX_ elementos del total)",
-        infoPostFix:    "",
-        loadingRecords: "Procesamiento en curso...",
-        zeroRecords:    "No hay elementos para mostrar",
-        emptyTable:     "No se encotraron elementos",
-        paginate: {
-            first:      "Primera",
-            previous:   "Anterior",
-            next:       "Próxima",
-            last:       "Ültima"
-        },
-        aria: {
-            sortAscending:  ": activar para ordenar la columna en orden ascendente",
-            sortDescending: ": activar para ordenar la columna en orden descendente"
+$(document).ready(function() {
+    $('.table-normal').DataTable( {
+          responsive: true,
+          ordering: false,
+          language: {
+            processing:     "Procesamiento en courso...",
+            search:         "Buscar&nbsp;:",
+            lengthMenu:     "Mostrar _MENU_ &eacute;l&eacute;mentos",
+            info:           "Mostrar de elelemento _START_ al _END_,     Total _TOTAL_ ",
+            infoEmpty:      "Visualización del elemento 0 a 0 de 0 artículos",
+            infoFiltered:   "(filtrados _MAX_ elementos del total)",
+            infoPostFix:    "",
+            loadingRecords: "Procesamiento en curso...",
+            zeroRecords:    "No hay elementos para mostrar",
+            emptyTable:     "No se encotraron elementos",
+            paginate: {
+                first:      "Primera",
+                previous:   "Anterior",
+                next:       "Próxima",
+                last:       "Ültima"
+            },
+            aria: {
+                sortAscending:  ": activar para ordenar la columna en orden ascendente",
+                sortDescending: ": activar para ordenar la columna en orden descendente"
+            }
         }
-    }
+    });
 });
 
 $("#myModal").on("click", ".close-modal", function () {

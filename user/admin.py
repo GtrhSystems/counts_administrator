7c64725972d2a4f9807ba6ba3af489f9b3a407b0
juator <juator@example.com>
2024-06-10 16:44:59 -0500

    ajuste para creacion de usuario con contraseña y edicion

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Permission
-
+from .forms import CustomUserChangeForm, CustomUserCreationForm
 
 admin.site.site_header = "El Gamer Mexicano  Administrador"
 admin.site.site_title = "El Gamer Mexicano  Administrador"
@@ -25,23 +25,35 @@ PERMISSIONS_ACTIVES = [
 ]
 
 class UserAdmin(admin.ModelAdmin):
-
+    #update_form = CustomUserChangeForm  # Formulario para cambiar usuarios
+    #form = CustomUserCreationForm  # Formulario para crear usuarios
     list_display = ('username', 'is_active', 'first_name', 'last_name', 'email')
     #fields = ['first_name', 'last_name', 'email',  'is_active']
-    fieldsets = (
-        ('Información personal', {'fields': ('username','first_name', 'last_name', 'email')}),
-        ('Permisos de usuario', {'fields': ( 'last_login', 'user_permissions')}),
-    )
-    #readonly_fields = ['first_name', 'last_name', 'email']
-    list_filter = ('is_staff', 'is_superuser')
-    search_fields = ('username__startswith', 'email__startswith')
-    filter_horizontal = ('groups', 'user_permissions',)
+
     def formfield_for_manytomany(self, db_field, request=None, **kwargs):
         if db_field.name == "user_permissions":
             kwargs["queryset"] = Permission.objects.filter(codename__in=PERMISSIONS_ACTIVES)
         return super().formfield_for_manytomany(db_field, request, **kwargs)
 
+    fieldsets = (
+        ('Información personal', {'fields': ('username','first_name', 'last_name', 'email')}),
+        ('Permisos de usuario', {'fields': ( 'last_login', 'user_permissions')}),
+        ('Seguridad', {'fields': ('password1', 'password2')}),
+    )
 
+    #readonly_fields = ['first_name', 'last_name', 'email']
+    list_filter = ('is_staff', 'is_superuser')
+    search_fields = ('username__startswith', 'email__startswith')
+    #filter_horizontal = ('groups', 'user_permissions',)
+
+
+
+    def get_form(self, request, obj=None, **kwargs):
+
+        if obj:
+            return CustomUserChangeForm
+        else:
+            return CustomUserCreationForm
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
--- user/forms.py
+++ user/forms.py
@@ -1,7 +1,24 @@
 from django import forms
 from .models import Customer
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, ReadOnlyPasswordHashField
+from django.contrib.auth.models import User, Permission
+
+PERMISSIONS_ACTIVES = [
+    'change_platform',
+    'add_customer',
+    'change_customer',
+    'add_user',
+    'add_count',
+    'change_count',
+    'delete_count',
+    'change_count',
+    'add_promotion',
+    'delete_sale',
+    'add_sale',
+    'add_plan',
+    'change_plan',
+    'delete_customer'
+]
 
 class UserForm(UserCreationForm):
 
@@ -20,3 +37,37 @@ class CustomerForm(forms.ModelForm):
     class Meta:
         model = Customer
         fields = ['name', 'phone' ]
+
+class CustomUserChangeForm(UserChangeForm):
+    password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirmar contraseña', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_login', 'user_permissions', 'last_name', 'email', 'password1', 'password2','is_active', 'is_staff']
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Contraseñas no coinciden")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    user_permissions = forms.ModelMultipleChoiceField(queryset=Permission.objects.filter(codename__in=PERMISSIONS_ACTIVES),
+                                              widget=forms.SelectMultiple,
+                                              required=False )
+    class Meta:
+        model = User
+        fields = ['username', 'last_login', 'first_name', 'user_permissions',  'last_name', 'email', 'password1', 'password2']

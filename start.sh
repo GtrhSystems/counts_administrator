c5c6d2614219ff42d90a7b69a6f8999dc733331b
root <root@vmi867744.contaboserver.net>
2024-08-12 14:07:21 -0500

    Primer commit - migración desde VPS

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 NAME="Administrador_de_cuentas"                                  # Name of the application
-NUM_WORKERS=3
+NUM_WORKERS=49
 DJANGODIR=/home/counts_administrator            # Django project directory
 DJANGO_SETTINGS_MODULE=count_admin_project.settings             # which settings file should Django use
 DJANGO_WSGI_MODULE=count_admin_project.wsgi
@@ -10,4 +10,5 @@ echo "Starting $NAME as `whoami`"
 . $DJANGODIR/env/bin/activate
 #export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 cd $DJANGODIR
-exec gunicorn ${DJANGO_WSGI_MODULE}:application --name $NAME  --threads=1 --workers $NUM_WORKERS --bind 0.0.0.0:8001
+#exec gunicorn ${DJANGO_WSGI_MODULE}:application --name $NAME  --threads=1 --workers $NUM_WORKERS --bind 0.0.0.0:8001
+exec gunicorn ${DJANGO_WSGI_MODULE}:application --name $NAME --workers $NUM_WORKERS  --bind 0.0.0.0:8001
